915a3f7959fc8c3546a86afebcce41e312422d5c
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2019-01-06 12:32:53 +0100

    Version Bump - 0.2.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
     kw = {}
 
 setup(name='gast',  # gast, daou naer!
-      version='0.2.0',
+      version='0.2.1',
       packages=['gast'],
       description='Python AST that abstracts the underlying Python version',
       long_description='''
