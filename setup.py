e86c4413542031268832d0fa97a1b71b8f0bc4ec
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2021-08-02 07:44:02 +0200

    0.5.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
     kw = {}
 
 setup(name='gast',  # gast, daou naer!
-      version='0.5.1',
+      version='0.5.2',
       packages=['gast'],
       description='Python AST that abstracts the underlying Python version',
       long_description='''
